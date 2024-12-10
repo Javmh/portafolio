e538e531d0733b0deed6fa09d6928d23e87524a9
Javmh <javier.ocyt@gmail.com>
2024-12-10 12:05:39 -0500

    Actualizacion de app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import init_routes  # Importamos las rutas desde routes.py
+import os
 
 # Crear la aplicación Flask
 app = Flask(__name__)
@@ -9,4 +10,7 @@ init_routes(app)
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
-    app.run(debug=True)
+        # Usa el puerto proporcionado por Render o un valor predeterminado (5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
